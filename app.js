aab72ad39b627651bc080a3e73c31e2f1c378773
Ahmed Ali <ali0792@gmail.com>
2021-04-29 14:50:32 -0500

    On Querying With Models

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const morgan = require("morgan");
-
+const main = require("./views/main.js");
 const app = express();
+const { db, Page, User } = require("./models");
+
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send(console.log("Hello World!"));
+db.authenticate().then(() => {
+  console.log("connected to the database");
 });
+
+app.get("/", (req, res) => {
+  res.send(main(""));
+});
+
+const PORT = 1337;
+
+const init = async () => {
+  await db.sync({ force: true });
+  // make sure that you have a PORT constant
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+
+init();
+
+// app.listen(PORT, () => {
+//   console.log(`app listening in PORT ${PORT}`);
+// });
